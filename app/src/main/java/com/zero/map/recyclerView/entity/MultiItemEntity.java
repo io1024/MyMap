4fcc0c5a6deea0dca6e984734ede64fff2de7918
wangming <fuming1109@gmail.com>
2019-10-06 14:37:09 +0800

    集成百度地图

---
+++ app/src/main/java/com/zero/map/recyclerView/entity/MultiItemEntity.java
@@ -0,0 +1,10 @@
+package com.zero.map.recyclerView.entity;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public interface MultiItemEntity {
+
+    int getItemType();
+
+}
